ddc6dbf0c76455e5611e603cec3ec254b12634fc
CodeBloodedDeveloper <20ec072@jssaten.ac.in>
2022-11-13 00:02:18 +0530

    latest commit

--- CodeMen/urls.py
+++ CodeMen/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
