bbf24d2a37f6a2d269bc651ff1451b51d602a69d
CodeBloodedDeveloper <20ec072@jssaten.ac.in>
2022-11-12 23:24:47 +0530

    latest commit

--- CodeMen/settings.py
+++ CodeMen/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT =
+STATIC_ROOT = BASE_DIR / 'assets'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
