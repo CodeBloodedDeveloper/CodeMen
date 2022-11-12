5816c49009b8274c2a17b95ac1492694a3cd2d85
CodeBloodedDeveloper <20ec072@jssaten.ac.in>
2022-11-12 22:41:33 +0530

    latest commit

--- CodeMen/urls.py
+++ CodeMen/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/', include('base.api.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_URL}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
